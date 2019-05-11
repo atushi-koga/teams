a7eff5b205c90416350b6df86c67c917f6d965d4
atsushi.koga <atsushi.koga@coprosystem.co.jp>
2019-05-11 12:19:11 +0900

    add: heaer and table html

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,18 +1,17 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>トップ</title>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    <a href="{{ route('') }}">新規会員登録</a>
-                    <a href="{{ route('') }}">ログイン</a>
-                </div>
-        </div>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <title>トップ</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+  <div class="content">
+    <div class="title m-b-md">
+      <a href="{{ route('') }}">新規会員登録</a> <a href="{{ route('') }}">ログイン</a>
+    </div>
+  </div>
+</body>
 </html>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,18 +1,28 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>トップ</title>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    <a href="{{ route('showRegistrationForm') }}">新規会員登録</a>
-                    <a href="{{ route('showLoginForm') }}">ログイン</a>
-                </div>
-        </div>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <title>トップ</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/css/base.css">
+  <link rel="stylesheet" href="/css/header.css">
+  {{--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">--}}
+</head>
+<body>
+<div class="header">
+  <div>
+    登山しようよ
+  </div>
+  <div>
+    <ul class="header-menu">
+      <li>
+        <a href="{{ route('showRegistrationForm') }}">新規会員登録</a>
+      </li>
+      <li>
+        <a href="{{ route('showLoginForm') }}">ログイン</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</body>
 </html>
