eb375dcc13a6dfde5f798d37442ff122cf3bf181
atsushi.koga <atsushi.koga@coprosystem.co.jp>
2019-06-17 16:55:16 +0900

    update jenkins job

--- job.sh
+++ job.sh
@@ -2,7 +2,25 @@ docker-compose up -d
 docker exec apache-php composer install
 docker exec apache-php php artisan migrate --seed
 
-bash job_script.sh ; RETURN=$?
+docker exec apache-php ./vendor/bin/phpcs --report=checkstyle --report-file=result/test_phpcs.xml ./app/Http/Controllers/Controller.php
+
+var1=$?
+array=($var1)
+
+docker exec apache-php ./vendor/bin/phpunit --log-junit result/phpunit.xml .
+
+var2=$?
+array+=($var2)
 
 docker-compose down
-exit $RETURN
+
+for i in ${array[@]}
+do
+  if [ $i != 0 ]; then
+    echo 'fail'
+    exit 1
+  fi
+done
+
+echo 'success'
+exit 0
