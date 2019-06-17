186647caa442e6d814e5086044c4fa263f825214
atsushi.koga <atsushi.koga@coprosystem.co.jp>
2019-06-17 16:43:51 +0900

    run PHP_CodeSniffer in jenkins job

--- job.sh
+++ job.sh
@@ -1,4 +1,8 @@
 docker-compose up -d
+docker exec apache-php composer install
+docker exec apache-php php artisan migrate --seed
+
 bash job_script.sh ; RETURN=$?
+
 docker-compose down
 exit $RETURN
