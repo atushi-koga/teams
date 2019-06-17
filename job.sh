63e7ebc0f5121815bed7da06c8786a8ec1afe0c1
atsushi.koga <atsushi.koga@coprosystem.co.jp>
2019-06-17 17:00:57 +0900

    update jenkins job

--- job.sh
+++ job.sh
@@ -7,7 +7,7 @@ docker exec apache-php ./vendor/bin/phpcs --report=checkstyle --report-file=resu
 var1=$?
 array=($var1)
 
-docker exec apache-php ./vendor/bin/phpunit --log-junit result/phpunit.xml .
+docker exec apache-php ./vendor/bin/phpunit --log-junit result/phpunit.xml
 
 var2=$?
 array+=($var2)
